49256f90322918208465fe8c9aae5524216ce44b
ieinberg <ieinberg@kth.se>
2024-12-04 12:27:53 +0100

    Generate login page

--- apps/web/src/routeTree.gen.ts
+++ apps/web/src/routeTree.gen.ts
@@ -10,158 +10,180 @@
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as IndexImport } from "./routes/index";
-import { Route as DummyIndexImport } from "./routes/dummy/index";
-import { Route as CoursesIndexImport } from "./routes/courses/index";
-import { Route as CoursesCourseIdIndexImport } from "./routes/courses/$courseId/index";
-import { Route as AccountsAccountIdIndexImport } from "./routes/accounts/$accountId/index";
+import { Route as rootRoute } from './routes/__root'
+import { Route as IndexImport } from './routes/index'
+import { Route as LoginIndexImport } from './routes/login/index'
+import { Route as DummyIndexImport } from './routes/dummy/index'
+import { Route as CoursesIndexImport } from './routes/courses/index'
+import { Route as CoursesCourseIdIndexImport } from './routes/courses/$courseId/index'
+import { Route as AccountsAccountIdIndexImport } from './routes/accounts/$accountId/index'
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
+
+const LoginIndexRoute = LoginIndexImport.update({
+  id: '/login/',
+  path: '/login/',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const DummyIndexRoute = DummyIndexImport.update({
-  id: "/dummy/",
-  path: "/dummy/",
+  id: '/dummy/',
+  path: '/dummy/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const CoursesIndexRoute = CoursesIndexImport.update({
-  id: "/courses/",
-  path: "/courses/",
+  id: '/courses/',
+  path: '/courses/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const CoursesCourseIdIndexRoute = CoursesCourseIdIndexImport.update({
-  id: "/courses/$courseId/",
-  path: "/courses/$courseId/",
+  id: '/courses/$courseId/',
+  path: '/courses/$courseId/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const AccountsAccountIdIndexRoute = AccountsAccountIdIndexImport.update({
-  id: "/accounts/$accountId/",
-  path: "/accounts/$accountId/",
+  id: '/accounts/$accountId/',
+  path: '/accounts/$accountId/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/courses/": {
-      id: "/courses/";
-      path: "/courses";
-      fullPath: "/courses";
-      preLoaderRoute: typeof CoursesIndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/dummy/": {
-      id: "/dummy/";
-      path: "/dummy";
-      fullPath: "/dummy";
-      preLoaderRoute: typeof DummyIndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/accounts/$accountId/": {
-      id: "/accounts/$accountId/";
-      path: "/accounts/$accountId";
-      fullPath: "/accounts/$accountId";
-      preLoaderRoute: typeof AccountsAccountIdIndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/courses/$courseId/": {
-      id: "/courses/$courseId/";
-      path: "/courses/$courseId";
-      fullPath: "/courses/$courseId";
-      preLoaderRoute: typeof CoursesCourseIdIndexImport;
-      parentRoute: typeof rootRoute;
-    };
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/courses/': {
+      id: '/courses/'
+      path: '/courses'
+      fullPath: '/courses'
+      preLoaderRoute: typeof CoursesIndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/dummy/': {
+      id: '/dummy/'
+      path: '/dummy'
+      fullPath: '/dummy'
+      preLoaderRoute: typeof DummyIndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/login/': {
+      id: '/login/'
+      path: '/login'
+      fullPath: '/login'
+      preLoaderRoute: typeof LoginIndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/accounts/$accountId/': {
+      id: '/accounts/$accountId/'
+      path: '/accounts/$accountId'
+      fullPath: '/accounts/$accountId'
+      preLoaderRoute: typeof AccountsAccountIdIndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/courses/$courseId/': {
+      id: '/courses/$courseId/'
+      path: '/courses/$courseId'
+      fullPath: '/courses/$courseId'
+      preLoaderRoute: typeof CoursesCourseIdIndexImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/courses": typeof CoursesIndexRoute;
-  "/dummy": typeof DummyIndexRoute;
-  "/accounts/$accountId": typeof AccountsAccountIdIndexRoute;
-  "/courses/$courseId": typeof CoursesCourseIdIndexRoute;
+  '/': typeof IndexRoute
+  '/courses': typeof CoursesIndexRoute
+  '/dummy': typeof DummyIndexRoute
+  '/login': typeof LoginIndexRoute
+  '/accounts/$accountId': typeof AccountsAccountIdIndexRoute
+  '/courses/$courseId': typeof CoursesCourseIdIndexRoute
 }
 
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/courses": typeof CoursesIndexRoute;
-  "/dummy": typeof DummyIndexRoute;
-  "/accounts/$accountId": typeof AccountsAccountIdIndexRoute;
-  "/courses/$courseId": typeof CoursesCourseIdIndexRoute;
+  '/': typeof IndexRoute
+  '/courses': typeof CoursesIndexRoute
+  '/dummy': typeof DummyIndexRoute
+  '/login': typeof LoginIndexRoute
+  '/accounts/$accountId': typeof AccountsAccountIdIndexRoute
+  '/courses/$courseId': typeof CoursesCourseIdIndexRoute
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute;
-  "/": typeof IndexRoute;
-  "/courses/": typeof CoursesIndexRoute;
-  "/dummy/": typeof DummyIndexRoute;
-  "/accounts/$accountId/": typeof AccountsAccountIdIndexRoute;
-  "/courses/$courseId/": typeof CoursesCourseIdIndexRoute;
+  __root__: typeof rootRoute
+  '/': typeof IndexRoute
+  '/courses/': typeof CoursesIndexRoute
+  '/dummy/': typeof DummyIndexRoute
+  '/login/': typeof LoginIndexRoute
+  '/accounts/$accountId/': typeof AccountsAccountIdIndexRoute
+  '/courses/$courseId/': typeof CoursesCourseIdIndexRoute
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
+  fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | "/"
-    | "/courses"
-    | "/dummy"
-    | "/accounts/$accountId"
-    | "/courses/$courseId";
-  fileRoutesByTo: FileRoutesByTo;
+    | '/'
+    | '/courses'
+    | '/dummy'
+    | '/login'
+    | '/accounts/$accountId'
+    | '/courses/$courseId'
+  fileRoutesByTo: FileRoutesByTo
   to:
-    | "/"
-    | "/courses"
-    | "/dummy"
-    | "/accounts/$accountId"
-    | "/courses/$courseId";
+    | '/'
+    | '/courses'
+    | '/dummy'
+    | '/login'
+    | '/accounts/$accountId'
+    | '/courses/$courseId'
   id:
-    | "__root__"
-    | "/"
-    | "/courses/"
-    | "/dummy/"
-    | "/accounts/$accountId/"
-    | "/courses/$courseId/";
-  fileRoutesById: FileRoutesById;
+    | '__root__'
+    | '/'
+    | '/courses/'
+    | '/dummy/'
+    | '/login/'
+    | '/accounts/$accountId/'
+    | '/courses/$courseId/'
+  fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  CoursesIndexRoute: typeof CoursesIndexRoute;
-  DummyIndexRoute: typeof DummyIndexRoute;
-  AccountsAccountIdIndexRoute: typeof AccountsAccountIdIndexRoute;
-  CoursesCourseIdIndexRoute: typeof CoursesCourseIdIndexRoute;
+  IndexRoute: typeof IndexRoute
+  CoursesIndexRoute: typeof CoursesIndexRoute
+  DummyIndexRoute: typeof DummyIndexRoute
+  LoginIndexRoute: typeof LoginIndexRoute
+  AccountsAccountIdIndexRoute: typeof AccountsAccountIdIndexRoute
+  CoursesCourseIdIndexRoute: typeof CoursesCourseIdIndexRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   CoursesIndexRoute: CoursesIndexRoute,
   DummyIndexRoute: DummyIndexRoute,
+  LoginIndexRoute: LoginIndexRoute,
   AccountsAccountIdIndexRoute: AccountsAccountIdIndexRoute,
   CoursesCourseIdIndexRoute: CoursesCourseIdIndexRoute,
-};
+}
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
 
 /* ROUTE_MANIFEST_START
 {
@@ -172,6 +194,7 @@ export const routeTree = rootRoute
         "/",
         "/courses/",
         "/dummy/",
+        "/login/",
         "/accounts/$accountId/",
         "/courses/$courseId/"
       ]
@@ -184,6 +207,9 @@ export const routeTree = rootRoute
     },
     "/dummy/": {
       "filePath": "dummy/index.tsx"
+    },
+    "/login/": {
+      "filePath": "login/index.tsx"
     },
     "/accounts/$accountId/": {
       "filePath": "accounts/$accountId/index.tsx"
