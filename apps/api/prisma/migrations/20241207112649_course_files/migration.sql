d465f8199735589e9f28e3cc2b19047b909a882e
Hampus Hallkvist <hampus@hallkvist.org>
2024-12-07 12:27:17 +0100

    migration: Add course files
    Co-authored-by: ieinberg <ieinberg@kth.se>

--- apps/api/prisma/migrations/20241207112649_course_files/migration.sql
+++ apps/api/prisma/migrations/20241207112649_course_files/migration.sql
@@ -2,32 +2,6 @@
 CREATE TYPE "GradingScale" AS ENUM ('AF', 'PF');
 
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "surname" TEXT NOT NULL,
-    "verified" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Token" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Course" (
     "id" TEXT NOT NULL,
     "courseCode" TEXT NOT NULL,
@@ -85,22 +59,10 @@ CREATE TABLE "Examiner" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Token_token_key" ON "Token"("token");
-
--- CreateIndex
-CREATE INDEX "Token_token_idx" ON "Token"("token");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Course_courseCode_key" ON "Course"("courseCode");
 
 -- CreateIndex
 CREATE INDEX "Course_courseCode_idx" ON "Course"("courseCode");
-
--- AddForeignKey
-ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Examination" ADD CONSTRAINT "Examination_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE SET NULL ON UPDATE CASCADE;
