731cbed8dcdd4b86234277a7fd88cd344fa66833
Hampus Hallkvist <hampus@hallkvist.org>
2024-12-07 13:12:51 +0100

    fix: http client fix

--- apps/web/src/utilities/http-client.ts
+++ apps/web/src/utilities/http-client.ts
@@ -1,4 +1,4 @@
 import type { App } from "$api/index";
 import { treaty } from "@elysiajs/eden";
 
-export const api = treaty<App>("localhost:3000").api;
+export const api = treaty<App>("localhost:3000");
