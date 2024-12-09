315a639359926ef54345e65bdbcbe70d8b7398cd
Hampus Hallkvist <hampus@hallkvist.org>
2024-12-09 20:42:59 +0100

    fix: Change url from hard coded to dynamic env

--- apps/web/src/utilities/http-client.ts
+++ apps/web/src/utilities/http-client.ts
@@ -1,4 +1,4 @@
 import type { App } from "$api/index";
 import { treaty } from "@elysiajs/eden";
 
-export const api = treaty<App>("localhost:3000");
+export const api = treaty<App>(import.meta.env.VITE_API_URL as string);
