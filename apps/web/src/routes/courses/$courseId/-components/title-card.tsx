9d94e5498fa227f569c90329ae5001c75fbe4629
ieinberg <ieinberg@kth.se>
2024-12-20 15:20:25 +0100

    Add number of ratings ticker to title card

--- apps/web/src/routes/courses/$courseId/-components/title-card.tsx
+++ apps/web/src/routes/courses/$courseId/-components/title-card.tsx
@@ -3,14 +3,15 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import NumberTicker from "@/components/ui/number-ticker";
 import { Star, StarHalf } from "lucide-react";
 
-function RatingStars(rating: number) {
+function RatingStars(rating: number, numRatings: number) {
   const fraction = rating % 1;
   const includeHalfStar = 0.25 < fraction && fraction < 0.75; // Include half star if rating fraction between .25-.75
   const numFilled = Math.floor(rating) + (fraction > 0.75 ? 1 : 0);
   return (
     <div className="flex flex-col self-center">
       <h2 className="relative self-center text-lg">
-        {<NumberTicker value={rating} decimalPlaces={2} />}
+        {<NumberTicker value={rating} decimalPlaces={2} />} (
+        <NumberTicker value={numRatings} />)
       </h2>
       <div className="flex">
         <div className="relative flex">
@@ -37,7 +38,7 @@ export function TitleCard({ courseData }: { courseData: CourseDto }) {
     <Card className="max-w-[480px]">
       <CardHeader className="bottom-0 flex flex-row justify-between pb-1">
         <h1 className="text-5xl">{courseData.code}</h1>
-        {RatingStars(courseData.rating ?? 0)}
+        {RatingStars(courseData.rating ?? 0, courseData.reviewCount ?? 0)}
       </CardHeader>
       <CardContent className="top-0 mt-0">
         <h2 className="text-lg">{courseData.title}</h2>
