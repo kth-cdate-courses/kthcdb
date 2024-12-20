08fdea5864d60472679453a93198d4003e1317cc
ieinberg <ieinberg@kth.se>
2024-12-20 15:30:56 +0100

    Update title card styling

--- apps/web/src/routes/courses/$courseId/-components/title-card.tsx
+++ apps/web/src/routes/courses/$courseId/-components/title-card.tsx
@@ -1,5 +1,5 @@
 import { CourseDto } from "$api/routes/courses/course-dto";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import NumberTicker from "@/components/ui/number-ticker";
 import { Star, StarHalf } from "lucide-react";
 
@@ -8,7 +8,7 @@ function RatingStars(rating: number, numRatings: number) {
   const includeHalfStar = 0.25 < fraction && fraction < 0.75; // Include half star if rating fraction between .25-.75
   const numFilled = Math.floor(rating) + (fraction > 0.75 ? 1 : 0);
   return (
-    <div className="flex flex-col self-center">
+    <div className="relative flex flex-col self-center pt-1">
       <h2 className="relative self-center text-lg">
         {<NumberTicker value={rating} decimalPlaces={2} />} (
         <NumberTicker value={numRatings} />)
@@ -35,12 +35,12 @@ function RatingStars(rating: number, numRatings: number) {
 }
 export function TitleCard({ courseData }: { courseData: CourseDto }) {
   return (
-    <Card className="max-w-[480px]">
+    <Card className="max-w-[400px]">
       <CardHeader className="bottom-0 flex flex-row justify-between pb-1">
-        <h1 className="text-5xl">{courseData.code}</h1>
+        <CardTitle className="text-5xl">{courseData.code}</CardTitle>
         {RatingStars(courseData.rating ?? 0, courseData.reviewCount ?? 0)}
       </CardHeader>
-      <CardContent className="top-0 mt-0">
+      <CardContent className="-mt-4">
         <h2 className="text-lg">{courseData.title}</h2>
       </CardContent>
     </Card>
