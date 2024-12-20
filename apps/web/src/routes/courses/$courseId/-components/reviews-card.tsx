39ef0133fe8afe0d77e73eb68f31d258995996aa
ieinberg <ieinberg@kth.se>
2024-12-20 10:59:30 +0100

    Add rating selector to ReviewsCard

--- apps/web/src/routes/courses/$courseId/-components/reviews-card.tsx
+++ apps/web/src/routes/courses/$courseId/-components/reviews-card.tsx
@@ -6,7 +6,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Star } from "@/components/ui/star";
 import { ReviewCard } from "@/routes/-components/review-card";
+import { Asterisk } from "lucide-react";
+import { useState } from "react";
 
 function reviewCardCB(review: ReviewDto) {
   return <ReviewCard key={review.id} {...review} />;
@@ -25,8 +35,27 @@ export function ReviewsCard({ data }: { data: ReviewDto[] | null }) {
   const reviews = data;
   return (
     <Card className="flex max-h-[480px] w-[320px] flex-col">
-      <CardHeader>
-        <CardTitle>Reviews</CardTitle>
+      <CardHeader className="flex flex-row justify-between">
+        <CardTitle>
+          <h1>Reviews</h1>
+        </CardTitle>
+        <Select>
+          <SelectTrigger id="rating" className="w-[80px]">
+            <SelectValue placeholder={<Asterisk className="w-4" />} />
+          </SelectTrigger>
+          <SelectContent position="popper" align="center">
+            {[1, 2, 3, 4, 5].map((it) => (
+              <SelectItem
+                key={it}
+                value={it.toString()}
+                className="flex items-center justify-start gap-3"
+              >
+                <span className="text-center">{it}</span>
+                <Star filled className="h-5 w-5" />
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </CardHeader>
       <CardContent className="block max-h-full gap-2 overflow-scroll">
         {reviews.map(reviewCardCB)}
