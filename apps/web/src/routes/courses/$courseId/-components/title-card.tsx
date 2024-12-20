b0f827f71b0e48f0be3e9c5e206a6c57dca97d87
ieinberg <ieinberg@kth.se>
2024-12-20 14:47:40 +0100

    Add smart star rating to title card
    Number of stars shown: Ratings with fractions .0-.25 are rounded down, .25-.75 rounded down but shown with half star, and 0.75-0.99... rounded up

--- apps/web/src/routes/courses/$courseId/-components/title-card.tsx
+++ apps/web/src/routes/courses/$courseId/-components/title-card.tsx
@@ -1,13 +1,41 @@
 import { CourseDto } from "$api/routes/courses/course-dto";
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Star, StarHalf } from "lucide-react";
 
+function RatingStars(rating: number) {
+  const fraction = rating % 1;
+  const includeHalfStar = 0.25 < fraction && fraction < 0.75; // Include half star if rating fraction between .25-.75
+  const numFilled = Math.floor(rating) + (fraction > 0.75 ? 1 : 0);
+  return (
+    <div className="flex self-center">
+      <div className="flex">
+        {Array(5)
+          .fill(0)
+          .map((_, index) => (
+            <Star key={"full" + index} fill="grey" strokeWidth={0} />
+          ))}
+      </div>
+      <div className="absolute flex">
+        {Array(numFilled)
+          .fill(0)
+          .map((_, index) => (
+            <Star key={"empty" + index} fill="gold" strokeWidth={0} />
+          ))}
+        {includeHalfStar && <StarHalf fill="gold" strokeWidth={0} />}
+      </div>
+    </div>
+  );
+}
 export function TitleCard({ courseData }: { courseData: CourseDto }) {
   return (
-    <Card className="w-[480px]">
-      <CardHeader>
+    <Card className="max-w-[480px]">
+      <CardHeader className="bottom-0 flex flex-row justify-between pb-1">
         <h1 className="text-5xl">{courseData.code}</h1>
-        <h2 className="text-lg">{courseData.title}</h2>
+        {RatingStars(courseData.rating ?? 0)}
       </CardHeader>
+      <CardContent className="top-0 mt-0">
+        <h2 className="text-lg">{courseData.title}</h2>
+      </CardContent>
     </Card>
   );
 }
