d72b18481ae9320d2cc48cc14af6125a8c85c77b
ieinberg <ieinberg@kth.se>
2024-12-21 17:02:51 +0100

    Fix overflow issues

--- apps/web/src/routes/accounts/$accountId/-components/reviews-card.tsx
+++ apps/web/src/routes/accounts/$accountId/-components/reviews-card.tsx
@@ -19,7 +19,7 @@ export function ReviewsCard({ userData }: { userData: UserDto }) {
       <CardHeader className="flex items-center gap-4">
         <CardTitle className="text-xl">Recent reviews</CardTitle>
       </CardHeader>
-      <CardContent className="mx-auto grid grid-cols-2 gap-4">
+      <CardContent className="mx-auto grid grid-cols-2 gap-4 overflow-auto">
         {userData.reviews.map((review) => (
           <ReviewCard key={review.id} {...review} />
         ))}
