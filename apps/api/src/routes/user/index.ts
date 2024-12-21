72b0067400d1be5d588af7f64387fabe92097a5a
ieinberg <ieinberg@kth.se>
2024-12-21 16:54:11 +0100

    Add reviews to user endpoint

--- apps/api/src/routes/user/index.ts
+++ apps/api/src/routes/user/index.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/utilities/db";
 import Elysia, { t } from "elysia";
 import { UserDto } from "./user-dto";
+import { ReviewDto } from "../courses/review/review-dto";
 
 export const userRoute = new Elysia({
   prefix: "/user",
@@ -15,16 +16,32 @@ export const userRoute = new Elysia({
         name: true,
         surname: true,
         createdAt: true,
+        reviews: true,
       },
     });
     if (!userInfo) {
       return null;
     }
+
     return {
       id: userId,
       name: userInfo.name,
       surname: userInfo.surname,
       userCreatedDate: userInfo.createdAt,
+      reviews: userInfo.reviews.map(
+        (review) =>
+          ({
+            id: review.id,
+            courseCode: "NULL NULL NULL TODO TODO MAYDAY THIS REQUIRES A FIX", // TODO: course code not provided
+            courseRoundId: review.courseRoundId,
+            rating: review.rating,
+            body: review.body,
+            createdAt: review.createdAt,
+            updatedAt: review.updatedAt,
+            userId: review.userId,
+            author: `${userInfo.name} ${userInfo.surname}`,
+          }) satisfies ReviewDto,
+      ),
     } satisfies UserDto;
   },
   {
--- apps/api/src/routes/user/user-dto.ts
+++ apps/api/src/routes/user/user-dto.ts
@@ -1,9 +1,11 @@
 import z from "zod";
+import { reviewDtoSchema } from "../courses/review/review-dto";
 
 export const userDtoSchema = z.object({
   id: z.string(),
   name: z.string(),
   surname: z.string(),
   userCreatedDate: z.date(),
+  reviews: z.array(reviewDtoSchema),
 });
 export type UserDto = z.infer<typeof userDtoSchema>;
