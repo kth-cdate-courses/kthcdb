08ffe46c6b6c81a48b998904dd5239440850cf2d
ieinberg <ieinberg@kth.se>
2024-12-21 15:12:05 +0100

    Add name to profile card

--- apps/web/src/routes/accounts/$accountId/-components/profile-card.tsx
+++ apps/web/src/routes/accounts/$accountId/-components/profile-card.tsx
@@ -4,12 +4,13 @@ import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 export function ProfileCard() {
   return (
     <Card className="mx-4 flex h-[80vh] w-3/12 flex-col justify-between">
-      <CardHeader className="flex gap-4">
+      <CardHeader className="flex items-center gap-4">
         <img
           className="h-[160px] w-[160px] self-center rounded-full"
           alt="User profile picture"
           src="/favicon.ico"
         />
+        <p className="text-xl">Inga Ingesta</p>
         <p className="rounded-sm border-[1px] border-zinc-200 bg-zinc-50 p-2 text-base">
           Autem quia est excepturi rerum quae aut. Omnis voluptatum cum
           voluptatem illo asperiores. Nihil consequatur quas eveniet mollitia ut
