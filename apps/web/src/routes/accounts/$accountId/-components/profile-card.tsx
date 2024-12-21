57f28bc749b637a8371e5842bec2a2ae9384fd7d
ieinberg <ieinberg@kth.se>
2024-12-21 15:07:15 +0100

    Add ProfileCard

---
+++ apps/web/src/routes/accounts/$accountId/-components/profile-card.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+
+export function ProfileCard() {
+  return (
+    <Card className="mx-4 flex h-[80vh] w-3/12 flex-col justify-between">
+      <CardHeader className="flex gap-4">
+        <img
+          className="h-[160px] w-[160px] self-center rounded-full"
+          alt="User profile picture"
+          src="/favicon.ico"
+        />
+        <p className="rounded-sm border-[1px] border-zinc-200 bg-zinc-50 p-2 text-base">
+          Autem quia est excepturi rerum quae aut. Omnis voluptatum cum
+          voluptatem illo asperiores. Nihil consequatur quas eveniet mollitia ut
+          dolorum. Explicabo ab repellat at velit. Qui porro porro et est
+          eligendi et. At repellendus voluptatem ullam sint praesentium.
+        </p>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full bg-[rgb(0,0,97)]">Log out</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- apps/web/src/routes/accounts/$accountId/index.tsx
+++ apps/web/src/routes/accounts/$accountId/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { ProfileCard } from "./-components/profile-card";
 
 export const Route = createFileRoute("/accounts/$accountId/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <p>Some user</p>;
+  return (
+    <div className="my-auto flex h-dvh flex-row items-center">
+      <ProfileCard></ProfileCard>
+    </div>
+  );
 }
