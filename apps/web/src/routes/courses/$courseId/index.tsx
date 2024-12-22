bb9a10a6339f0f538cb78575a60985acd47cc14a
ieinberg <ieinberg@kth.se>
2024-12-22 14:36:49 +0100

    Remove examination card and corresponding dummy data

--- apps/web/src/routes/courses/$courseId/index.tsx
+++ apps/web/src/routes/courses/$courseId/index.tsx
@@ -59,45 +59,6 @@ export const Route = createFileRoute("/courses/$courseId/")({
   }),
 });
 
-/* const dummyRounds: ExaminationRound[] = [
-  {
-    examCode: "LAB1",
-    title: "Laboratory assignments",
-    gradeScaleCode: "AF",
-    credits: 4,
-    creditUnitLabel: "Credits",
-    creditUnitAbbr: "hp",
-    ladokUID: "398bbf86-73d8-11e8-b4e0-063f9afb40e3",
-  },
-  {
-    examCode: "MAS1",
-    title: "Individual master's test",
-    gradeScaleCode: "AF",
-    credits: 1.5,
-    creditUnitLabel: "Credits",
-    creditUnitAbbr: "hp",
-    ladokUID: "398bbf86-73d8-11e8-b4e0-063f9afb40e3",
-  },
-  {
-    examCode: "MAS2",
-    title: "Individual master's test",
-    gradeScaleCode: "AF",
-    credits: 1.5,
-    creditUnitLabel: "Credits",
-    creditUnitAbbr: "hp",
-    ladokUID: "398bbf86-73d8-11e8-b4e0-063f9afb40e3",
-  },
-  {
-    examCode: "TEN1",
-    title: "Theory examination",
-    gradeScaleCode: "PF",
-    credits: 2.5,
-    creditUnitLabel: "Credits",
-    creditUnitAbbr: "hp",
-    ladokUID: "398bbf86-73d8-11e8-b4e0-063f9afb40e3",
-  },
-]; */
-
 function RouteComponent() {
   const { user } = useSession();
   const navigate = useNavigate({
