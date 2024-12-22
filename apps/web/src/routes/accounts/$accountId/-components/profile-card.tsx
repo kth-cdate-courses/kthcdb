e872fe7cca604048c0465d2307c06fbb7216da47
ieinberg <ieinberg@kth.se>
2024-12-22 17:47:58 +0100

    Remove logout button from profile page

--- apps/web/src/routes/accounts/$accountId/-components/profile-card.tsx
+++ apps/web/src/routes/accounts/$accountId/-components/profile-card.tsx
@@ -1,13 +1,10 @@
 import { UserDto } from "$api/routes/user/user-dto";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { useSession } from "@/utilities/useSession";
 
 export function ProfileCard({ userData }: { userData: UserDto }) {
-  const { logout } = useSession();
   return (
     <Card className="w-full">
-      <CardContent className="flex flex-col items-center pb-4 pt-12">
+      <CardContent className="flex flex-col items-center pt-12">
         <p className="mx-auto text-3xl">
           {userData.name} {userData.surname}
         </p>
@@ -19,12 +16,6 @@ export function ProfileCard({ userData }: { userData: UserDto }) {
           })}
         </p>
       </CardContent>
-      <CardFooter className="pt-0">
-        {" "}
-        <Button className="w-full" onClick={logout}>
-          Log out
-        </Button>
-      </CardFooter>
     </Card>
   );
 }
